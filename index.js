a44dab07df41d093ea293d927c7856a7eab8993b
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-06-01 12:37:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,160 @@
+require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+
+
+// middleware
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}));
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@assignment-12.moncfbx.mongodb.net/?retryWrites=true&w=majority&appName=Assignment-12`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Token related kaj
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            // console.log("Token er user", user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+        // Middleware
+        const verifyToken = (req, res, next) => {
+            console.log("Inside verify token: --", req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "unauthorized access" });
+            }
+
+            const token = req.headers.authorization.split(" ")[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: "unauthorized access" });
+                }
+                req.decoded = decoded;
+                next()
+            })
+
+        }
+
+
+        const spotsCollection = client.db("tourism").collection('touristSpots');
+        const wishListCollection = client.db("tourism").collection('wishlist');
+        const tourGuidesCollection = client.db("tourism").collection('tourGuides');
+        const packageBookingCollection = client.db("tourism").collection('packageBooking');
+        const usersCollection = client.db("tourism").collection('users');
+
+        // get all spots in db
+        app.get('/spots', async (req, res) => {
+            const result = await spotsCollection.find().toArray()
+            res.send(result)
+        })
+
+        // get specific spot with id
+        app.get('/spot/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await spotsCollection.findOne(query);
+            res.send(result)
+        })
+
+        // add wishlist in DB
+        app.post('/wishlist', async (req, res) => {
+            const spot = req.body;
+            const result = await wishListCollection.insertOne(spot);
+            res.send(result)
+        })
+
+        // get all tourGuides data
+        app.get('/tourGuides', async (req, res) => {
+            const result = await tourGuidesCollection.find().toArray();
+            res.send(result)
+        })
+
+        // get specific tourGuides data with id
+        app.get('/tourGuide/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await tourGuidesCollection.findOne(query);
+            res.send(result)
+        })
+
+        // get just tour guide name
+        app.get('/tourGuidesName', async (req, res) => {
+            const options = {
+                projection: { _id: 0, name: 1 }
+            }
+            const result = await tourGuidesCollection.find({}, options).toArray()
+            res.send(result)
+        })
+
+        // post packageBookin Info in db
+        app.post('/packageBooking', async (req, res) => {
+            const bookingInfo = req.body;
+            const result = await packageBookingCollection.insertOne(bookingInfo);
+            res.send(result)
+        })
+
+        // newUserAdd DB
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            // check now. If user already exist than return now . Otherwise add now
+            const query = { userEmail: userInfo.userEmail }
+            const isExist = await usersCollection.findOne(query);
+            if (isExist) {
+                return res.send({ message: "This user already exist in DB", insertedId: null })
+            }
+            const result = await usersCollection.insertOne(userInfo)
+            res.send(result)
+        })
+
+        // fixed kono user er role get korbo
+        app.get('/userRole/:email', async(req, res)=> {
+            const email = req.params.email;
+            const query= {userEmail: email}
+            const options = {
+                projection: {_id: 0, userRole: 1}
+            }
+            const result = await usersCollection.findOne(query, options)
+            res.send(result)
+        })
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send("Your server is Running")
+})
+
+app.listen(port, () => {
+    console.log("Your Tourist server is Running in port", port)
+})
