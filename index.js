60fd184b9e7d0ba8096c18229c6125d58493d622
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-06-03 15:19:36 -0700

    booking related api

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ async function run() {
             res.send(result)
         })
 
+        // get specific type spots in db
+        app.get('/spots/:tourType', async (req, res) => {
+            const tourType = req.params.tourType;
+            const query = {tourType: tourType}
+            const result = await spotsCollection.find(query).toArray()
+            res.send(result)
+        })
+
         // get specific spot with id
         app.get('/spot/:id', async (req, res) => {
             const id = req.params.id;
@@ -108,6 +116,14 @@ async function run() {
             res.send(result)
         })
 
+        // get all booking data a tourGuide email
+        app.get('/requestedTourGuide/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {guideEmail: email}
+            const result = await packageBookingCollection.find(query).toArray();
+            res.send(result)
+        })
+
         // get specific user all booking
         app.get('/myBookings/:email', async (req, res) => {
             const email = req.params.email;
@@ -143,7 +159,6 @@ async function run() {
         app.patch('/updateUser/:email', async (req, res) => {
             const email = req.params.email;
             const updateInfo = req.body;
-
             // if status === pending . tahole return now
             const filter = { userEmail: email }
             const find = await usersCollection.findOne(filter);
@@ -157,16 +172,30 @@ async function run() {
             const updatedDoc = {
                 $set: {
                     userStatus: "Pending",
-                    phoneNumber: '43243',
-                    education: 'gdf',
-                    skills: 'sfdsfsf',
-                    age: '543',
-                    experience: 'dfgd',
-                    timestamp: Date.now()
+                    timestamp: Date.now(),
+                    ...updateInfo
                 }
             }
             const result = await usersCollection.updateOne(query, updatedDoc, options);
             res.send(result)
+        })
+
+        // review add usersCollection review array
+        app.patch('/review/:email', async(req, res)=> {
+            const email = req.params.email;
+            const review = req.body;
+            // console.log(email, review)
+            const query = {userEmail: email};
+            const result = await usersCollection.findOne(query);
+            const reviews = result?.reviews;
+            reviews.push(review)
+            const updatedDoc = {
+                $set: {
+                    reviews: reviews
+                }
+            }
+            const updateResult = await usersCollection.updateOne(query, updatedDoc)
+            res.send(updateResult)
         })
 
         // fixed kono user er role get korbo
@@ -198,7 +227,7 @@ async function run() {
         // get just tour guide name
         app.get('/tourGuidesName', async (req, res) => {
             const options = {
-                projection: { _id: 0, userName: 1 }
+                projection: { _id: 0, userEmail: 1, userName: 1 }
             }
             const result = await usersCollection.find({}, options).toArray()
             res.send(result)
